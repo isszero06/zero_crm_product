efe9cd2bca2af67982c838aff6d45c60b8a0c368
isszero06 <76986925+isszero06@users.noreply.github.com>
2023-03-15 23:13:03 +0200

    Update __manifest__.py

--- zero_crm_product/__manifest__.py
+++ zero_crm_product/__manifest__.py
@@ -9,18 +9,50 @@
 #
 #################################################################################
 {
-    'name': "CRM Products",
+    'name': "CRM opportunity Products",
 
     'summary': """
-        Add Products to opportunity/Lead""",
+        CRM opportunity leads Products""",
 
     'description': """
-        Add Products to opportunity/Lead
+        Add Products and Services to CRM Lead / Opportunity
+Add any Product Type "Storable Product - Consumable - Service " To lead/Opportunity in CRM
+Support Price List related CRM users defined by default in user partner ID profile. 
+Opportunity Expected Revenue Computed by Net Total Amount "Products/Services Lines"
+Duplicate opportunity will Duplicate All Products Line .
+Add Note Line And Section Line to Opportunity and Transfer to Quotation with Products Lines.
+Pricelist Changed Automatics related Customer if customer defined to lead/Opportunity.
+Support Multi Currency and currency automatics related Price List Currency.
+Support Fiscal Position.
+Add Payment Terms Related Opportunity
+New menu To Manage Products, Product Variants, Products Attribute and Pricelist  Also from CRM.
+When Create Quotation from Opportunity System will transfer All New Fields to Quotation.
+#odoo
+#branch
+odoo 16 accounting
+odoo accounting
+odoo course
+odoo pos
+erp odoo
+crm odoo
+odoo 16 inventory
+odoo implementation
+odoo inventory
+#odoo
+#odoo 
+#crmp 
+#crm 
+#opportunity 
+#products 
+#car 
+#service 
+#center 
+#realestate
     """,
 
     'author': 'Zero Systems',
     'website': "http://erpzero.com",
-    # 'live_test_url': 'https://youtu.be/L94RQeFL_w8',
+    'live_test_url': 'https://youtu.be/643WhbrZ0IE',
     'category': 'Sales',
     'version': '0.6',
     'depends': ['sale_crm'],
