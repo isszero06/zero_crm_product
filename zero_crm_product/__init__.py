61f828c0577eeade91cb3a41d5e1b779097e2672
Zero Systems <isszero06@gmail.com>
2024-01-17 06:48:27 +0200

    Update __init__.py

--- zero_crm_product/__init__.py
+++ zero_crm_product/__init__.py
@@ -9,14 +9,13 @@
 #
 #################################################################################
 
-from odoo import SUPERUSER_ID, api
 
 from . import models
 
-def pre_init_check_vers(cr):
+def pre_init_check(cr):
     from odoo.service import common
-    from odoo.exceptions import Warning
+    from odoo.exceptions import UserError
     version_info = common.exp_version()
     server_serie =version_info.get('server_serie')
-    if server_serie!='16.0':raise Warning('Module support Odoo series 16.0 found {}.'.format(server_serie))
+    if server_serie!='17.0':raise UserError('Module support Odoo series 17.0 found {}.'.format(server_serie))
     return True
